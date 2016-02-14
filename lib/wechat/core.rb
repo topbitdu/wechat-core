4e3e0785522077f42d8020ed25171f01ad5c79c0
Topbit Du <topbit.du@gmail.com>
2016-02-14 15:53:49 +0800

    1, Initial Commit.

---
+++ lib/wechat/core.rb
@@ -0,0 +1,7 @@
+require "wechat/core/version"
+
+module Wechat
+  module Core
+    # Your code goes here...
+  end
+end
---
+++ lib/wechat/core/version.rb
@@ -0,0 +1,5 @@
+module Wechat
+  module Core
+    VERSION = '0.1'.freeze
+  end
+end

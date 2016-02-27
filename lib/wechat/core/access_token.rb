15e602ba1f949f30ab708f44d8991a14317abcf7
Topbit Du <topbit.du@gmail.com>
2016-02-27 17:04:46 +0800

    1, Deprecated the ::load method, use the ::create method instead.

--- lib/wechat/core/access_token.rb
+++ lib/wechat/core/access_token.rb
@@ -21,4 +21,27 @@ class Wechat::Core::AccessToken
     message.body
   end
 
+  # 获取 Access Token
+  # http://mp.weixin.qq.com/wiki/11/0e4b294685f817b95cbed85ba5e82b8f.html
+  #
+  # Return hash format if success:
+  # {
+  #   access_token: <ACCESS_TOKEN>,
+  #   expires_in: 7200
+  # }
+  # The ACCESS_TOKEN is 107 characters in 2015.
+  def self.create(app_id, app_secret)
+    message = ::JSONClient.new.get 'https://api.weixin.qq.com/cgi-bin/token',
+      {
+        grant_type: 'client_credential',
+        appid:      app_id,     # Rails.application.secrets.wechat_app_id,
+        secret:     app_secret, # Rails.application.secrets.wechat_app_secret
+      }
+    message.body
+  end
+
+  class << self
+    deprecate load: :create, deprecator: ActiveSupport::Deprecation.new('1.0', 'wechat-core')
+  end
+
 end

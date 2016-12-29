e2de749c28b2e699d85af2cd335871f451c8ffd7
Topbit Du <topbit.du@gmail.com>
2016-12-29 12:08:32 +0800

    1, Improve the Follower Profile wrapper class for the document.

--- lib/wechat/core/follower_profile.rb
+++ lib/wechat/core/follower_profile.rb
@@ -2,6 +2,7 @@ class Wechat::Core::FollowerProfile
 
   extend Wechat::Core::Common
 
+  ##
   # 批量获取用户基本信息
   # http://mp.weixin.qq.com/wiki/14/bb5031008f1494a59c6f71fa0f319c66.html#.E6.89.B9.E9.87.8F.E8.8E.B7.E5.8F.96.E7.94.A8.E6.88.B7.E5.9F.BA.E6.9C.AC.E4.BF.A1.E6.81.AF
   # 最多支持一次拉取100条。
@@ -20,7 +21,7 @@ class Wechat::Core::FollowerProfile
   #       country:        <COUNTRY_NAME>,
   #       headimgurl:     <HEAD_IMAGE_LINK>,
   #       subscribe_time: <SUBSCRIBE_TIME>,
-  #       unionid:        <UNION_ID>, 
+  #       unionid:        <UNION_ID>,
   #       remark:         <REMARK>,
   #       groupid:        <GROUP_ID>
   #     }
@@ -39,7 +40,7 @@ class Wechat::Core::FollowerProfile
 
   # 获取用户基本信息(UnionID机制)
   # http://mp.weixin.qq.com/wiki/14/bb5031008f1494a59c6f71fa0f319c66.html#.E8.8E.B7.E5.8F.96.E7.94.A8.E6.88.B7.E5.9F.BA.E6.9C.AC.E4.BF.A1.E6.81.AF.EF.BC.88.E5.8C.85.E6.8B.ACUnionID.E6.9C.BA.E5.88.B6.EF.BC.89
-  # 一次拉取调用最多拉取10000个关注者的OpenID，可以通过多次拉取的方式来满足需求。 
+  # 一次拉取调用最多拉取10000个关注者的OpenID，可以通过多次拉取的方式来满足需求。
   #
   # Return hash format if success:
   # {
